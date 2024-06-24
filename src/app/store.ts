2a3ade1319a224657f61532e98b94fef5bdd5c0f
parkyooni <elmi1003@naver.com>
2024-06-24 16:09:48 +0900

    Feat: slice.ts에 counter중에서 증가선언 & store.ts에서 내보냄 및 자동업뎃 기능추가

---
+++ src/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+// Redux Create Store Redux Dev Tools Extenstions 자동으로 on.
+// Thunk 미들웨어를 자동으로 추가 및 우발적인 돌연병이와 같은 일반적인 실수를 감지하고, 몇 가지 개발 검사를 자동으로 활성화.
+import counterReducer from "../features/counter/counter-slice";
+
+export const store = configureStore({
+  reducer: {
+    // Combine Reducer가 호출되어 state에 "state.counterfield" 생성
+    counter: counterReducer,
+  },
+});
+
+// TS 선언으로 dispatch를 store에서 내보냄
+export type AppDispatch = typeof store.dispatch;
+
+// 해당 저장소에 더 많은 slice reducer를 추가하면, 해당 유형이 자동으로 업뎃 처리해줌
+export type Rootsate = ReturnType<typeof store.getState>; // <typeof store.getState>; TS의 내장객체
---
+++ src/features/counter/counter-slice.ts
@@ -0,0 +1,42 @@
+// single file
+// Redux는 기본적으로 4개의 형태를 가지며, 해당 프로젝트는 단일 파일로 작성 예정으로 이를 slice-file로 지칭 -> 이유 : Redux의 한조각에 대한 논리와 데이터를 나타내기 위함
+
+// [ DUCKS Pattern ]
+// D
+// U
+// C
+// K
+// S
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// createSlice -> Redux 논리정의
+// PayloadAction -> TypeScript 유형
+
+// slice는 숫자로 다룸
+interface CounterSate {
+  value: number;
+}
+// slice 초기상태 정의
+const initialState: CounterSate = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    // increment
+    // incremented : inline obj function
+    incremented(state) {
+      // it's okay todo this because immer makes it mmutable under the nood
+      state.value += 1;
+    },
+    // decrement
+    // reset
+  },
+});
+
+// createSlice an Other
+// 1. reducer 기능인 [slice audio]
+
+export const { incremented } = counterSlice.actions;
+export default counterSlice.reducer;
